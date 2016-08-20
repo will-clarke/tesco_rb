9a07b91ab75606a30172e37bd7556e6149e9f2fa
William <wmmclarke@gmail.com>
2016-08-20 16:49:08 +0100

    Readme & removed fake tests

--- test/tesco_rb/item_test.rb
+++ test/tesco_rb/item_test.rb
@@ -2,16 +2,19 @@ require 'test_helper'
 
 class ItemTest < Minitest::Test
 
-  @client = ::TescoRb::Client.new(ENV['TESCO_API_KEY'])
+  # TODO: Create these tests
+  # Also, potentailly create '.search' syntax:
+  #    client.item.search('bananas')
 
-  client.item.search('bananas')
-  # -- same as:
-  client.item(query: 'bananas')
-  client.item(offset: 0, limit: 10, query: 'chives')
+  # @client = ::TescoRb::Client.new(ENV['TESCO_API_KEY'])
+  # client.item.search('bananas')
+  # # -- same as:
+  # client.item(query: 'bananas')
+  # client.item(offset: 0, limit: 10, query: 'chives')
 
-  assert_kind_of Array, client.store
-  assert_kind_of TescoRb::Store, client.store.first
+  # assert_kind_of Array, client.store
+  # assert_kind_of TescoRb::Store, client.store.first
 
-  assert_equal client.store(limit: 1).count, 1
-  assert_equal client.store(limit: 5).count, 5
+  # assert_equal client.store(limit: 1).count, 1
+  # assert_equal client.store(limit: 5).count, 5
 end
