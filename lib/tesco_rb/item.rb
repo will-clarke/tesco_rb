eb7e035c91ebbb5c7777faf2edeca6339bacf068
William <wmmclarke@gmail.com>
2016-08-24 22:05:30 +0100

    Items get populated dynamically :tada:

--- lib/tesco_rb/item.rb
+++ lib/tesco_rb/item.rb
@@ -5,23 +5,36 @@ module TescoRb
   class Item
     include RepresentationHelper
 
-    def self.fields
-      %I( image tpnb PromotionDescription
-          ContentsMeasureType name UnitOfSale
-          AverageSellingUnitWeight UnitQuantity
-          ContentsQuantity price unitprice)
-    end
-
-    attr_reader(*fields)
-
-    def initialize(location)
-      set self.class.fields, location
+    def initialize(json)
+      item = json.each.with_object({}) { |(k, v), hash| hash[underscore(k)] = v }
+      create_attr_readers_for item.keys
+      set item.keys, item
     end
 
     def ==(other)
-      self.class.fields.all? do |method_name|
-        send(method_name) == other.send(method_name)
+      instance_variables.all? do |method_name|
+        current_var = instance_variable_get method_name.to_sym
+        other_var = other.instance_variable_get method_name.to_sym
+        current_var == other_var
       end
+    end
+
+    private
+
+    def create_attr_readers_for(keys)
+      keys.map! { |i| underscore(i) }
+      keys.each do |key|
+        self.class.class_eval("def #{key};@#{key};end")
+      end
+    end
+
+    # kind of nabbed from ActiveSupport
+    def underscore(word)
+      word.to_s.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
     end
 
   end
--- test/tesco_rb/item_test.rb
+++ test/tesco_rb/item_test.rb
@@ -1,24 +1,19 @@
 require 'test_helper'
 
 class ItemTest < Minitest::Test
+  def test_dynamic_initialization
+    bob = TescoRb::Item.new(is: 'awesome')
+    assert_equal bob.is, 'awesome'
+    chris = TescoRb::Item.new(thinks_he_is: 'cool', actually: 'isnt')
+    assert_equal chris.thinks_he_is, 'cool'
+    assert_equal chris.actually, 'isnt'
+  end
 
-  # to test: 1: dynamic initialization
-  #          2: equality
-
-
-  # TODO: Create these tests
-  # Also, potentailly create '.search' syntax:
-  #    client.item.search('bananas')
-
-  # @client = ::TescoRb::Client.new(ENV['TESCO_API_KEY'])
-  # client.item.search('bananas')
-  # # -- same as:
-  # client.item(query: 'bananas')
-  # client.item(offset: 0, limit: 10, query: 'chives')
-
-  # assert_kind_of Array, client.store
-  # assert_kind_of TescoRb::Store, client.store.first
-
-  # assert_equal client.store(limit: 1).count, 1
-  # assert_equal client.store(limit: 5).count, 5
+  def test_equality
+    bob = TescoRb::Item.new(is: 'awesome')
+    not_bob = TescoRb::Item.new(is: 'loser')
+    close_enough = TescoRb::Item.new(is: 'awesome')
+    assert bob != not_bob
+    assert bob == close_enough
+  end
 end
